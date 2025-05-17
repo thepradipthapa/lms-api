8ea5f4532734651973b28fee5a33f50563d4d900
Pradip Thapa <codewithpradip.com@gmail.com>
2025-05-17 21:39:55 +0545

    feat: created fundamental app for backend structure

--- lms_api/settings.py
+++ lms_api/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "course",
+    "chapter",
+    "order",
+    "review",
+    "coupon",
+    "doubt",
 ]
 
 MIDDLEWARE = [
