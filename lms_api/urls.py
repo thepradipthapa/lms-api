e3b3006616209889d685905b1b1db2da29958dbe
Pradip Thapa <codewithpradip.com@gmail.com>
2025-05-18 21:18:59 +0545

    feat: implement testing api

--- lms_api/urls.py
+++ lms_api/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/courses/', include('course.urls')),
+    path('api/chapters/', include('chapter.urls')),
+    path('api/coupons/', include('coupon.urls')),
+    path('api/review/', include('review.urls')),
+    path('api/orders/', include('order.urls')),
+    path('api/doubts/', include('doubt.urls')),
 ]
