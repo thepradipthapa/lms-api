6cc1c0aca4c795a69276dba9795b45eabb2f49af
Pradip Thapa <codewithpradip.com@gmail.com>
2025-05-21 23:00:19 +0545

    feat: configure media file handling

--- lms_api/urls.py
+++ lms_api/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +30,4 @@ urlpatterns = [
     path('api/orders/', include('order.urls')),
     path('api/doubts/', include('doubt.urls')),
 ]
+if settings.DEBUG:urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
