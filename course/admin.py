6972b6697bfa165d0b7670ba5a01383b6e6c4116
Pradip Thapa <codewithpradip.com@gmail.com>
2025-05-21 22:30:38 +0545

    chore: register category and course model in Django admin

--- course/admin.py
+++ course/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Course, Category
 
 # Register models here.
-admin.register(Course)
-admin.register(Category) 
+admin.site.register(Course)
+admin.site.register(Category) 
