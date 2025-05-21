e533c5a104350426074f5d67a872fa7f91d22ebc
Pradip Thapa <codewithpradip.com@gmail.com>
2025-05-21 21:51:24 +0545

    add category model

--- course/models.py
+++ course/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Category(models.Model):
+    """ Represents a category for organizing coirses in the database."""
+
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    slug = models.SlugField()
+    title = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['title']
+
